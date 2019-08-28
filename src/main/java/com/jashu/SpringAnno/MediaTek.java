e0d8b100ee6bbb2048ef2d0112c7c3b80b3f5b11
jashu <jashubhai@dsquaresolutions.com>
2019-08-28 10:53:08 +0530

    primary annotation

---
+++ src/main/java/com/jashu/SpringAnno/MediaTek.java
@@ -0,0 +1,16 @@
+package com.jashu.SpringAnno;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class MediaTek implements MobileProcessor
+{
+
+    public void process()
+    {
+	System.out.println("2nd best cpu !!!");
+    }
+
+}
